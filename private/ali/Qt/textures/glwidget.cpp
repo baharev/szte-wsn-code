d8e9db293c002a663e9be4e42de298724d8387e9
baharev_ali <baharev_ali@a4e16f3a-606c-4714-833f-ed604d7aca08>
2011-01-07 19:50:11 +0000

    Rotation in Qt; simplifying textures example of Qt
    git-svn-id: https://svn.code.sf.net/p/szte-wsn/code@1473 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ private/ali/Qt/textures/glwidget.cpp
@@ -0,0 +1,172 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial Usage
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Nokia.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtGui>
+#include <QtOpenGL>
+
+#include "glwidget.h"
+
+GLWidget::GLWidget(QWidget *parent, QGLWidget *shareWidget)
+    : QGLWidget(parent, shareWidget)
+{
+    clearColor = Qt::black;
+    xRot = 0;
+    yRot = 0;
+    zRot = 0;
+}
+
+GLWidget::~GLWidget()
+{
+}
+
+QSize GLWidget::minimumSizeHint() const
+{
+    return QSize(50, 50);
+}
+
+QSize GLWidget::sizeHint() const
+{
+    return QSize(200, 200);
+}
+
+void GLWidget::rotate(double EulerX, double EulerY, double EulerZ)
+{
+    xRot = EulerX;
+    yRot = EulerY;
+    zRot = EulerZ;
+    updateGL();
+}
+
+void GLWidget::setClearColor(const QColor &color)
+{
+    clearColor = color;
+    updateGL();
+}
+
+void GLWidget::initializeGL()
+{
+    makeObject();
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+#ifndef QT_OPENGL_ES_2
+    glEnable(GL_TEXTURE_2D);
+#endif
+
+}
+
+void GLWidget::paintGL()
+{
+    qglClearColor(clearColor);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -10.0f);
+
+
+    glRotated(xRot, 1.0, 0.0, 0.0);
+    glRotated(yRot, 0.0, 1.0, 0.0);
+    glRotated(zRot, 0.0, 0.0, 1.0);
+
+    glVertexPointer(3, GL_FLOAT, 0, vertices.constData());
+    glTexCoordPointer(2, GL_FLOAT, 0, texCoords.constData());
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    for (int i = 0; i < 6; ++i) {
+        glBindTexture(GL_TEXTURE_2D, textures[i]);
+        glDrawArrays(GL_TRIANGLE_FAN, i * 4, 4);
+    }
+}
+
+void GLWidget::resizeGL(int width, int height)
+{
+    int side = qMin(width, height);
+    glViewport((width - side) / 2, (height - side) / 2, side, side);
+
+#if !defined(QT_OPENGL_ES_2)
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+#ifndef QT_OPENGL_ES
+    glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0);
+#else
+    glOrthof(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0);
+#endif
+    glMatrixMode(GL_MODELVIEW);
+#endif
+}
+
+void GLWidget::mousePressEvent(QMouseEvent *event)
+{
+    lastPos = event->pos();
+}
+
+void GLWidget::mouseReleaseEvent(QMouseEvent * /* event */)
+{
+    emit clicked();
+}
+
+void GLWidget::makeObject()
+{
+    static const int coords[6][4][3] = {
+        { { +1, -1, -1 }, { -1, -1, -1 }, { -1, +1, -1 }, { +1, +1, -1 } },
+        { { +1, +1, -1 }, { -1, +1, -1 }, { -1, +1, +1 }, { +1, +1, +1 } },
+        { { +1, -1, +1 }, { +1, -1, -1 }, { +1, +1, -1 }, { +1, +1, +1 } },
+        { { -1, -1, -1 }, { -1, -1, +1 }, { -1, +1, +1 }, { -1, +1, -1 } },
+        { { +1, -1, +1 }, { -1, -1, +1 }, { -1, -1, -1 }, { +1, -1, -1 } },
+        { { -1, -1, +1 }, { +1, -1, +1 }, { +1, +1, +1 }, { -1, +1, +1 } }
+    };
+
+    for (int j=0; j < 6; ++j) {
+        textures[j] = bindTexture
+            (QPixmap(QString(":/images/side%1.png").arg(j + 1)), GL_TEXTURE_2D);
+    }
+
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            texCoords.append
+                (QVector2D(j == 0 || j == 3, j == 0 || j == 1));
+            vertices.append
+                (QVector3D(0.2 * coords[i][j][0], 0.2 * coords[i][j][1],
+                           0.2 * coords[i][j][2]));
+        }
+    }
+}

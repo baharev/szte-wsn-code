2889713ccc242f93b7f8c14606a4e115989457a4
baharev_ali <baharev_ali@a4e16f3a-606c-4714-833f-ed604d7aca08>
2012-01-29 11:51:19 +0000

    R script, plotting
    git-svn-id: https://svn.code.sf.net/p/szte-wsn/code@2999 a4e16f3a-606c-4714-833f-ed604d7aca08

---
+++ private/ali/record_cut/R_plotter/plotter.R
@@ -0,0 +1,16 @@
+
+A <- as.matrix(read.csv("m005_r028_b7686_196797_233661.csv",header=FALSE));
+
+accx <- 3;
+
+fx <- array(A[ ,accx ]);
+
+dim(fx)
+
+#x  <- array(0:(dim(fx)-1));
+
+#dim(x)
+
+plot(A[,7])
+
+

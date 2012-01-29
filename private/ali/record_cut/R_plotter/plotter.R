aa39a395d90ef83971fa935f0e2063e32e60fb0b
baharev_ali <baharev_ali@a4e16f3a-606c-4714-833f-ed604d7aca08>
2012-01-29 21:15:38 +0000

    R script, plotting; list of TODOs
    git-svn-id: https://svn.code.sf.net/p/szte-wsn/code@3001 a4e16f3a-606c-4714-833f-ed604d7aca08

--- private/ali/record_cut/R_plotter/plotter.R
+++ private/ali/record_cut/R_plotter/plotter.R
@@ -1,16 +1,29 @@
 
 A <- as.matrix(read.csv("m005_r028_b7686_196797_233661.csv",header=FALSE));
 
-accx <- 3;
+r <- nrow(A)
 
-fx <- array(A[ ,accx ]);
 
-dim(fx)
+x <- c(1:r);
+x <- c(8000:22000);
 
-#x  <- array(0:(dim(fx)-1));
+accx <- c(A[x,4])
 
-#dim(x)
+plot( accx,type="l",col="red")
 
-plot(A[,7])
+accy <- c(A[x,5])
+accz <- c(A[x,6])
 
+lines(accy, type="l", col="green")
+lines(accz, type="l", col="blue")
+
+vx <- c( 9950, 9950);
+vy <- c(  500, 5000);
+
+lines(vx, vy, type="l", col="black");
+
+vx <- c( 9200, 9200)
+vy <- c(  500, 5000)
+
+lines(vx, vy, type="l", col="black")
 
